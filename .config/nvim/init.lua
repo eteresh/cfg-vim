930f4a656d484b4a226e3e210b43d8b26af5bf84
tereschenko.e13 <tereschenko.e13@wb.ru>
2024-06-27 12:39:24 +0300

    add lsp in lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,6 @@
+-- config based on:
+-- https://www.youtube.com/watch?v=6pAG3BHurdM
+-- https://www.josean.com/posts/how-to-setup-neovim-2024
 require('options')
 require('keymaps')
 require("config.lazy")
