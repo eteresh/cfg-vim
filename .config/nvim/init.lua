d5be78b3f6d276f298f00d8bf2aa98d7e1af7759
tereschenko.e13 <tereschenko.e13@wb.ru>
2024-08-25 16:56:53 +0300

    add autocmd to init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -12,3 +12,30 @@ vim.api.nvim_create_autocmd({ "TextYankPost" }, {
   end,
 }
 )
+
+vim.api.nvim_create_autocmd(
+    { "BufNewFile", "Bufread" }, {
+    pattern = { "*.ddl" },
+    callback = function()
+        local buf = vim.api.nvim_get_current_buf()
+        vim.api.nvim_buf_set_option(buf, "filetype", "sql")
+    end,
+  }
+)
+
+vim.api.nvim_create_autocmd(
+    { "BufReadPost" }, {
+    pattern = {
+      "*.h", "*.cpp", "*.c", "*.cc",
+      "*.js", "*.jsx", "*.ts", "*.tsx",
+      "*.html", "*.css",
+      "*.json",
+      "*.lua",
+      "*.sql", "*.ddl",
+    },
+    callback = function()
+      local buf = vim.api.nvim_get_current_buf()
+      vim.api.nvim_buf_set_option(buf, "shiftwidth", 2)
+    end,
+  }
+)
