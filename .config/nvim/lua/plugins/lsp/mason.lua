c00fac0c7297f2ac1e8cd9a75c78d3f1447da3ea
eugene <eteresh@gmail.com>
2025-06-19 13:06:15 +0300

    remove mason-lspconfig

--- .config/nvim/lua/plugins/lsp/mason.lua
+++ .config/nvim/lua/plugins/lsp/mason.lua
@@ -1,39 +1,12 @@
 return {
   "mason-org/mason.nvim",
-  dependencies = {
-    "mason-org/mason-lspconfig.nvim",
-  },
-  config = function()
-
-    -- import mason
-    local mason = require("mason")
-
-    -- import mason-lspconfig
-    local mason_lspconfig = require("mason-lspconfig")
-
-    mason.setup({
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗"
-        },
+  opts = {
+    ui = {
+      icons = {
+        package_installed = "✓",
+        package_pending = "➜",
+        package_uninstalled = "✗"
       },
-    })
-
-    mason_lspconfig.setup({
-      automatic_enable = true,
-      -- list of servers for mason to install
-      ensure_installed = {
-        "lua_ls",
-        "pyright",
-        "clangd",
-        "vimls",
-        "html",
-        "emmet_ls",
-        "cssls",
-        "fish_lsp",
-      },
-    })
-  end
+    },
+  }
 }
