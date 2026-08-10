12cf9af07676c53a2f4702b09c361d4e51753bf3
eugene <eteresh@gmail.com>
2026-08-10 13:54:11 +0300

    neopyter cmds

--- .config/nvim/lua/plugins/neopyter.lua
+++ .config/nvim/lua/plugins/neopyter.lua
@@ -18,12 +18,15 @@ return {
           map("n", "<Leader>ja", "<cmd>Neopyter execute notebook:run-all-above<cr>", "run all above cell")
           map("n", "<Leader>jb", "<cmd>Neopyter execute notebook:run-all-below<cr>", "run cell and all below")
           map("n", "<Leader>ji", "<cmd>Neopyter execute notebook:run-cell-and-insert-below<cr>", "run selected and insert below")
+          map("n", "<Leader>jr", "<cmd>Neopyter execute notebook:run-all<cr>", "run all above cell")
 
           map("n", "<Leader>jh", "<cmd>Neopyter execute notebook:hide-cell-code<cr>", "collapse cell code")
           map("n", "<Leader>jH", "<cmd>Neopyter execute notebook:hide-all-cell-code<cr>", "collapse all code")
 
           map("n", "<Leader>jc", "<cmd>Neopyter execute notebook:clear-cell-output<cr>", "clear-cell-output")
           map("n", "<Leader>jC", "<cmd>Neopyter execute notebook:clear-all-cell-outputs<cr>", "clear-cell-output")
+
+          map("n", "<Leader>jj", "<cmd>Neopyter command 127.0.0.1:9001<cr>", "Neopyter connect")
 
           map("n", "<Leader>j0", "<cmd>Neopyter execute kernelmenu:restart<cr>", "restart kernel")
           map("n", "<Leader>j9", "<cmd>Neopyter execute kernelmenu:restart-and-clear<cr>", "restart kernel and clear all cells")
